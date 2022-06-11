3eaf2bb9eacbb733cbf75c4ae2e26ddbf1044a7d
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-11 18:49:00 +1000

    added ability for user to change between on hover and on click for drawing lines

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,10 @@
 let currColor ="#000000"; 
 let isRainbow = false;
+let isClickRequired = false;
+
+let mouseDown = false
+document.body.onmousedown = () => {mouseDown = true}
+document.body.onmouseup = () => (mouseDown = false)
 
 function initSquareGrid(gridSize){
     let gridContainer = document.getElementById('grid');     
@@ -16,7 +21,9 @@ function initSquareGrid(gridSize){
             newDiv = document.createElement("div");
             newDiv.classList.add(`C${col}`, `R${row}`);
             newDiv.addEventListener("mouseover", onHover);
+            newDiv.addEventListener("mousedown", onHover);
             newDiv.style.backgroundColor = "white";
+            newDiv.style.userSelect = "none";
             gridContainer.appendChild(newDiv);
         }
     }
@@ -38,24 +45,46 @@ function onColorReset(){
     });
 }
 
-function onRainbow(){
-    isRainbow = !isRainbow;
-
-    rainbowButton = document.querySelector("#rainbow");
-    if (isRainbow){
-        rainbowButton.style.backgroundColor = "#e08169";
-        rainbowButton.style.color = "white";
+function changeButtonState(element, state){
+    if (state){
+        element.style.backgroundColor = "#e08169";
+        element.style.color = "white";
     } 
     else {
-        rainbowButton.style.backgroundColor = "#f2aca0";
-        rainbowButton.style.color = "black";
-        let prevColor = document.getElementById("color-picker").value;
-        currColor = prevColor; 
+        element.style.backgroundColor = "#f2aca0";
+        element.style.color = "black";
+    }
+}
+
+function onRainbow(){
+    isRainbow = !isRainbow;
+    changeButtonState(document.querySelector("#rainbow"), isRainbow);
+    
+    let prevColor = document.getElementById("color-picker").value;
+    currColor = prevColor; 
+}
+
+function onChangeClick(){
+    isClickRequired = !isClickRequired;
+    buttonContainer = document.getElementById("click-or-hover");
+    changeButtonState(buttonContainer, isClickRequired);
+    
+    if (isClickRequired){
+        buttonContainer.innerHTML = "CLICK";
+    }
+    else {
+        buttonContainer.innerHTML = "HOVER";
     }
 }
 
 function onHover(event){
-    event.srcElement.style.backgroundColor = currColor;
+    if (!isClickRequired){
+        event.srcElement.style.backgroundColor = currColor;
+    }
+    else if (!(event.type == "mouseover" && !mouseDown)){
+        event.srcElement.style.backgroundColor = currColor;
+    }
+
     if (isRainbow) {
         currColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     }
